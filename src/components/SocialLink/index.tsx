14de8b86598efe24b4c055518e2be205e63235c1
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-24 18:35:43 +0900

    Fix SocialLink component style.

--- src/components/SocialLink/index.tsx
+++ src/components/SocialLink/index.tsx
@@ -6,14 +6,26 @@ import svgInstagram from './images/instagram.svg';
 import svg500px from './images/500px.svg';
 import svgLastFm from './images/lastfm.svg';
 
+const socialLinks: {svg: string; url: string}[] = [
+  {svg: svgGitHub, url: 'https://github.com/sakai-akinobu'},
+  {svg: svgInstagram, url: 'https://www.instagram.com/sakai.akinobu/'},
+  {svg: svg500px, url: 'https://500px.com/nobyu'},
+  {svg: svgLastFm, url: 'http://www.lastfm.jp/user/nobyu'},
+];
+
 export default function SocialLink() {
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <img className={styles.logo} src={svgGitHub} />
-        <img className={styles.logo} src={svgInstagram} />
-        <img className={styles.logo} src={svg500px} />
-        <img className={styles.logo} src={svgLastFm} />
+        {
+          socialLinks.map(({svg, url}, index) => (
+            <div key={index} className={styles.logo}>
+              <a href={url}>
+                <img className={styles.svg} src={svg} />
+              </a>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
