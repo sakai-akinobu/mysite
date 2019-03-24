a8d6833b4c84aa811a8e568980a3a934e9153263
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-24 17:14:37 +0900

    Add sns logo images.

--- src/components/SocialLink/index.tsx
+++ src/components/SocialLink/index.tsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
 
+import styles from './index.scss';
+import svgGitHub from './images/github.svg';
+import svgInstagram from './images/instagram.svg';
+import svg500px from './images/500px.svg';
+import svgLastFm from './images/lastfm.svg';
+
 export default function SocialLink() {
-  return <div>Social Link</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img className={styles.logo} src={svgGitHub} />
+        <img className={styles.logo} src={svgInstagram} />
+        <img className={styles.logo} src={svg500px} />
+        <img className={styles.logo} src={svgLastFm} />
+      </div>
+    </div>
+  );
 }
