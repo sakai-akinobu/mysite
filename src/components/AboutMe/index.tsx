02e43e404ef59643b74f1a49736be3670ce7b923
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-24 15:37:33 +0900

    Fix AboutMe component.

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react';
 
+import styles from './index.scss';
+
 export default function AboutMe() {
-  return <div>About Me</div>;
+  return (
+    <section className={styles.container}>
+      <div>Hello, I&apos;m Akinobu!</div>
+      <div>A front-end developer based Tokyo, Japan.</div>
+    </section>
+  );
 }
