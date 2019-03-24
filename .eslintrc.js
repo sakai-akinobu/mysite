712f16b53c8efe87cd3eb3fad0088e77ef320aef
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-24 11:33:07 +0900

    Add .eslintrc.js file.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+  ],
+  rules: {
+    "@typescript-eslint/class-name-casing": "error",
+    "@typescript-eslint/member-delimiter-style": "error",
+    "@typescript-eslint/no-angle-bracket-type-assertion": "error",
+    "@typescript-eslint/type-annotation-spacing": "error",
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-empty-interface": "error",
+    "@typescript-eslint/no-triple-slash-reference": "error",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-use-before-define": "error",
+  },
+};
