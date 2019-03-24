3dfa707b978de535d37a0a642d55c53260960336
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-24 17:17:39 +0900

    Remove component.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import AboutMe from '../AboutMe';
 import TechnologyStack from '../TechnologyStack';
 import SocialLink from '../SocialLink';
-import ContactMe from '../ContactMe';
 import styles from './index.scss';
 
 export default function App() {
@@ -12,7 +11,6 @@ export default function App() {
       <AboutMe />
       <TechnologyStack />
       <SocialLink />
-      <ContactMe />
     </div>
   );
 }
