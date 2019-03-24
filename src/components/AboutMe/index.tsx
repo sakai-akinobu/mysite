b586407c4435a1376ac8398767bef99acb3ae7c7
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-24 18:25:44 +0900

    Fix AboutMe component style.

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -5,8 +5,11 @@ import styles from './index.scss';
 export default function AboutMe() {
   return (
     <section className={styles.container}>
-      <h1>Hello, I&apos;m Akinobu!</h1>
-      <p>A front-end developer based Tokyo, Japan.</p>
+      <div className={styles.inner}>
+        <h1 className={styles.hello}>Hello, I&apos;m Akinobu!</h1>
+        <p>Front-end developer based in Tokyo, Japan.</p>
+        <p>Front-end engineer interested in creating user interfaces. I has several years of experience as a back-end engineer. I like to make people&apos;s lives better with engineering.</p>
+      </div>
     </section>
   );
 }
