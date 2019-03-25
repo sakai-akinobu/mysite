782a321bc71014707b12cb3806aba2c1341878c2
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-26 07:26:13 +0900

    Fix webpack config.

--- webpack/development.js
+++ webpack/development.js
@@ -1,81 +1,10 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const shared = require('./shared');
 
 module.exports = Object.assign({}, shared, {
   mode: 'development',
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                importLoaders: 2,
-                sourceMap: true,
-                localIdentName: '[local]___[hash:base64:5]',
-              },
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                sourceMap: true,
-                plugins: [
-                  require('autoprefixer')(),
-                ],
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                outputStyle: 'expanded',
-                sourceMap: true,
-              },
-            },
-          ],
-        }),
-      },
-      {
-        test: /\.svg$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name]-[hash].[ext]',
-            outputPath: 'images',
-            publicPath: (path) => `/dist/images/${path}`,
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js',
-    ],
-  },
   devtool: 'inline-source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../public/dist'),
-  },
-  plugins: [
-    new ExtractTextPlugin('bundle.css'),
-  ],
   devServer: {
     publicPath: '/dist/',
     contentBase: path.join(__dirname, '../public'),
--- webpack/shared.js
+++ webpack/shared.js
@@ -1,4 +1,75 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 2,
+                sourceMap: true,
+                localIdentName: '[local]___[hash:base64:5]',
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  require('autoprefixer')(),
+                ],
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                outputStyle: 'expanded',
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.svg$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]-[hash].[ext]',
+            outputPath: 'images',
+            publicPath: (path) => `/dist/images/${path}`,
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js',
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../public/dist'),
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css'),
+  ],
 };
