becc4a9dcaa9567a1cf290eb7c90e659d48a4a0b
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-25 23:23:52 +0900

    Remove unnecessary elements.

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,14 +3,13 @@ import * as React from 'react';
 import AboutMe from '../AboutMe';
 import TechnologyStack from '../TechnologyStack';
 import SocialLink from '../SocialLink';
-import styles from './index.scss';
 
 export default function App() {
   return (
-    <div className={styles.container}>
+    <>
       <AboutMe />
       <TechnologyStack />
       <SocialLink />
-    </div>
+    </>
   );
 }
