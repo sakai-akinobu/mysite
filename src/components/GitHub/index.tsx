86e9cef8990eb65d4a9244362dd04152220198ee
sakai-akinobu <sakai.akinobu@gmail.com>
2019-03-30 08:40:48 +0900

    Show after loading.

--- src/components/GitHub/index.tsx
+++ src/components/GitHub/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useEffect, useRef} from 'react';
+import {useState, useEffect, useRef} from 'react';
 import Calendar from 'github-calendar';
 import 'github-calendar/dist/github-calendar.css';
 import 'github-calendar/dist/github-calendar-responsive.css';
@@ -8,15 +8,22 @@ import styles from './index.scss';
 
 export default function GitHub() {
   const calenderRef = useRef(null);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    new Calendar(calenderRef.current, 'sakai-akinobu', {responsive: true});
+    new Calendar(
+      calenderRef.current,
+      'sakai-akinobu',
+      {responsive: true}
+    ).then(() => {
+      setLoading(true);
+    });
   });
 
   return (
     <div className={styles.container}>
       <h2>GitHub contributions</h2>
       <div className={`transition ${styles.inner}`}>
-        <div className={`calendar ${styles.calendar}`} ref={calenderRef} />
+        <div className={`calendar ${styles.calendar} ${loading ? styles.loaded : ''}`} ref={calenderRef} />
       </div>
     </div>
   );
