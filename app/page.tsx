2effb1f4bc2b3768b449b4586cf38836db727069
sakai-akinobu <sakai.akinobu@gmail.com>
2024-11-06 16:25:57 +0900

    Remove TechnologyStack component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Script from "../src/components/Script";
 import AboutMe from "../src/components/AboutMe";
 import Footer from "../src/components/Footer";
-import TechnologyStack from "../src/components/TechnologyStack";
 
 const Page = () => {
   return (
     <>
       <Script />
       <AboutMe />
-      <TechnologyStack />
       <Footer />
     </>
   );
